3eb575a550f656b248a41103e98494daddc1f45c
Ibtahaj Ali Butt <ibtahajgrt@gmail.com>
2025-12-12 23:01:44 +0000

    Updated App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders project information header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // We search for text that actually exists in your App.js
+  const linkElement = screen.getByText(/Project Information/i);
   expect(linkElement).toBeInTheDocument();
 });
