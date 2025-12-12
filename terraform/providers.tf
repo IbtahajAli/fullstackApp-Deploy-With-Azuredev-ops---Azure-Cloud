068ccdb68d603396e1ae0845bf6cc4461ab9a9c8
Ibtahaj Ali Butt <ibtahajgrt@gmail.com>
2025-12-12 20:21:31 +0000

    Updated providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -18,9 +18,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  # Credentials are now inherited from the Azure DevOps Service Connection
 }
