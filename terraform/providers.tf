4ddf320763ebd028ad2f939456e720952f0d5443
IbtahajAli <88149767+IbtahajAli@users.noreply.github.com>
2025-12-12 15:47:11 +0300

    update provider file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,10 @@
-provider "azurerm" {
-  features {}
-}terraform {
+terraform {
   required_version = ">= 1.9.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"   # lock provider version for stability
+      version = "~> 3.100"
     }
   }
 
