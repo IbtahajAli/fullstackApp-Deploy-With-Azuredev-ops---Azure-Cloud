d7dfeb705be2ead93369dce8513e7bb931dfef99
Ibtahaj Ali Butt <ibtahajgrt@gmail.com>
2025-12-12 20:28:06 +0000

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,14 @@
 variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "East US"
+  type    = string
+  default = "East US"
 }
 
 variable "sql_admin_login" {
-  description = "SQL admin username"
-  type        = string
-  default     = "sqladmin"
+  type    = string
+  default = "sqladmin"
 }
 
 variable "sql_password" {
-  description = "SQL admin password"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
